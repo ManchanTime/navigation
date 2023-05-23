1bcfe1539d9c3306018bfaf3da16a37a7a1a9ec3
Lee <lych0918@gmail.com>
2023-05-23 18:54:45 +0900

    Wifi gps

--- app/src/main/java/com/gachon/innergation/activity/FindActivity.java
+++ app/src/main/java/com/gachon/innergation/activity/FindActivity.java
@@ -1,5 +1,6 @@
 package com.gachon.innergation.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -8,23 +9,59 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.gachon.innergation.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindActivity extends AppCompatActivity {
 
     TextView textName;
-
+    private String order;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ViewEx viewEx = new ViewEx(this);
-        setContentView(viewEx);
+        setContentView(R.layout.activity_find);
+        Intent get = getIntent();
+        order = get.getStringExtra("order");
+        set_up();
 //        textName = findViewById(R.id.text_name);
 //        Intent get = getIntent();
 //        textName.setText(get.getStringExtra("className"));
+    }
+
+    public void set_up(){
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = firebaseFirestore.collection("classrooms");
+        collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                        ArrayList<Map<String, Integer>> store = (ArrayList<Map<String, Integer>>) documentSnapshot.getData().get("RSSI");
+                        for(int i=0;i<store.size();i++) {
+                            for (Map.Entry<String, Integer> pair : store.get(i).entrySet()) {
+                                Log.e("key + value", pair.getKey() + " " + pair.getValue());
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        });
     }
 
     protected class ViewEx extends View
