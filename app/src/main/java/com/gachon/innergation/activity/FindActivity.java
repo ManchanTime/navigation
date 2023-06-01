6da9878873566a2620d6cb2aaf81b8b5560122b2
MoonJungGon <mtbird77@naver.com>
2023-06-01 15:03:11 +0900

    디자인 부분 수정

--- app/src/main/java/com/gachon/innergation/activity/FindActivity.java
+++ app/src/main/java/com/gachon/innergation/activity/FindActivity.java
@@ -21,10 +21,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -71,7 +68,6 @@ public class FindActivity extends AppCompatActivity {
     //목표 위치
     private String order;
 
-    private ImageView radar;
 
     // BroadcastReceiver 정의
     // 여기서는 이전 예제에서처럼 별도의 Java class 파일로 만들지 않았는데, 어떻게 하든 상관 없음
@@ -104,35 +100,33 @@ public class FindActivity extends AppCompatActivity {
 
         Collections.sort(wifiList);
 
+        Log.e("success", "success");
         for(int i=0;i<5;i++){
             comp.add(wifiList.get(i).getBssid());
             Log.e("test",wifiList.get(i).getSsid() + " " + wifiList.get(i).getBssid()+" " + wifiList.get(i).getRssi());
         }
-
         set_up();
     }
 
     private void scanFailure() {    // Wifi검색 실패
+        Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ViewEx viewEx = new ViewEx(this);
         setContentView(R.layout.activity_find);
 
-        radar=(ImageView)findViewById(R.id.radar);
-        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
-        radar.startAnimation(anim);
-
-
+        BackgroundThread thread = new BackgroundThread();
         requestRuntimePermission();
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        thread.start();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
         getApplicationContext().registerReceiver(wifiScanReceiverNow, intentFilter);
         // wifi가 활성화되어있는지 확인 후 꺼져 있으면 켠다
-        if(wifiManager.isWifiEnabled() == false)
+        if(wifiManager.isWifiEnabled() == false) {
             wifiManager.setWifiEnabled(true);
+        }
 
         //로딩창 객체 생성
         customProgressDialog = new CustomDialog(this);
@@ -218,23 +212,19 @@ public class FindActivity extends AppCompatActivity {
         });
     }
 
-    protected class ViewEx extends View{
-        public ViewEx(Context context)
-        {
-            super(context);
-        }
-        public void onDraw(Canvas canvas)
-        {
-            canvas.drawColor(Color.BLACK);
 
-            Paint MyPaint = new Paint();
-            MyPaint.setStrokeWidth(5f);
-            MyPaint.setStyle(Paint.Style.FILL);
-            MyPaint.setColor(Color.GRAY);
-            canvas.drawLine(0,0,360,640,MyPaint);
+    class BackgroundThread extends Thread{
+        public void run(){
+            while(true){
+                try{
+                    Thread.sleep(100);
+                }catch (Exception e){}
+                wifiList.clear();
+                // wifi 스캔 시작
+                wifiManager.startScan();
+            }
         }
     }
-
 
     //허용하시겠습니까? 퍼미션 창 뜨게하는 것!
     private void requestRuntimePermission() {
